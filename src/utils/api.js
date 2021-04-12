2ac23c3b4e92cec68a19dff62e7ba5e1a84a7264
TejaswiChillakuru <chillakuru.t@northeastern.edu>
2021-04-12 19:12:17 -0400

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getStudentsInfo = async () => {
-  const allSudents = await axios.get('/api?results=12');
+  const allSudents = await axios.get('https://randomuser.me/api?results=12');
   return (allSudents.data.results);
 };
 
